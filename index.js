cd94d11932c694436fdb4b8d30a5325e5c9854cf
Jack <jackn25@nycstudents.net>
2024-10-23 11:02:03 -0400

    Done with basic css and html working on js

--- index.js
+++ index.js
@@ -4,6 +4,31 @@
 let firstCard = 10
 let secondCard = 4
 
+//Part A and B in JS
+let cardHolder = document.createElement("p")
+cardHolder.id = "cards-el"
+let sumHolder = document.createElement("p")
+sumHolder.id = "sum-el"
+document.body.appendChild(cardHolder)
+document.body.appendChild(sumHolder)
+let startGame = document.createElement("button")
+let newCard = document.createElement("button")
+startGame.id = "startGame"
+newCard.id = "newCard"
+document.body.appendChild(startGame)
+document.body.appendChild(newCard)
+
+document.body.style.fontFamily = "Verdana, Geneva, Tahoma, sans-serif"
+document.body.style.backgroundImage = "images/table.png"
+document.body.style.textAlign = "center"
+document.body.style.color = "black"
+
+document.button.style.backgroundColor= "goldenrod";
+document.button.style.paddingtop = "8dp";
+document.button.style.margin = "10px";
+document.button.style.borderradius = "5px";
+document.button.style.width= "150px";
+
 //challenge #1
 //create a new variable and set it to the sum of the two cards 
 
