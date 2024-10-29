a933ab9ee5acfef135b73a34b06c6fa8d04441a3
Jack Ni <a@a>
2024-10-29 18:59:12 -0400

    Finished base

--- index.js
+++ index.js
@@ -2,53 +2,75 @@
 
 // variables representing two cards 
 let firstCard = 10
-let secondCard = 4
-
-//Part A and B in JS
-/*let cardHolder = document.createElement("p")
-cardHolder.id = "cards-el"
-let sumHolder = document.createElement("p")
-sumHolder.id = "sum-el"
-document.body.appendChild(cardHolder)
-document.body.appendChild(sumHolder)
-let startGame = document.createElement("button")
-let newCard = document.createElement("button")
-startGame.id = "startGame"
-newCard.id = "newCard"
-document.body.appendChild(startGame)
-document.body.appendChild(newCard)
-
-document.body.style.fontFamily = "Verdana, Geneva, Tahoma, sans-serif"
-document.body.style.background ="url('images/table.png')"
-document.body.style.textAlign = "center"
-document.body.style.color = "black"
-
-document.getElementById("startGame").style.backgroundColor= "goldenrod";
-document.getElementById("startGame").style.paddingtop = "8dp";
-document.getElementById("startGame").style.margin = "10px";
-document.getElementById("startGame").style.borderradius = "5px";
-document.getElementById("startGame").style.width= "150px";
-document.getElementById("startGame").innerHTML = "Start game"
-
-document.getElementById("newCard").style.backgroundColor= "goldenrod";
-document.getElementById("newCard").style.paddingtop = "8dp";
-document.getElementById("newCard").style.margin = "10px";
-document.getElementById("newCard").style.borderradius = "5px";
-document.getElementById("newCard").style.width= "150px";
-document.getElementById("newCard").innerHTML = "New card"*/
+let secondCard = 4 
 
 //challenge #1
 //create a new variable and set it to the sum of the two cards 
-let cards = []
-let sum = cards.sum
-let hasBlackjack = false
-let isAlive = true
-let messageEl = document.getElementById("message-el")
-let sumEl = document.getElementById("sum-el")
-let cardsEl = document.getElementById("cards-el")
-//chalenge #2 
+var cards = []
+var sum = 0
+var hasBlackjack = false
+var isAlive = true
+var messageEl = document.getElementById("message-el")
+var sumEl = document.getElementById("sum-el")
+var cardsEl = document.getElementById("cards-el")
+
+
+//challenge #2 
 // code the statements below based on the conditions shown in blackjack.png from the images folder
 // your output should work in the browser's console based on changing the values assigned to the cards
+
+function renderGame() {
+    cardsEl.textContent = "Cards: "
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i]
+        cardsEl.textContent += " "
+    }
+    sumEl.textContent = "Sum: " + sum
+    if (sum > 21) {
+        isAlive = false
+    } else if (sum < 21) {
+        isAlive = true
+    }
+    else {
+        hasBlackjack = true
+    }
+    if (hasBlackjack) {
+        messageEl.textContent = "Wohoo! You've got Blackjack! 🥳"
+    } else if (isAlive) {
+        messageEl.textContent = "Do you want to draw a new card? 🙂"
+    } else {
+        messageEl.textContent = "You're out of the game! 😭"
+    }
+}
+
+function newCard() {
+    let card = getRandomCard()
+    sum += card
+    cards.push(card)
+    console.log(cards)
+    renderGame()
+}
+
+function getRandomCard() {
+    let randomNumber = 1 + Math.floor(Math.random() * 13)
+    console.log(randomNumber)
+    if (randomNumber == 1) {
+        return 11
+    } else if (randomNumber > 9){
+        return 10
+    }
+    else return randomNumber
+}
+
+function startGame() {
+    isAlive = true
+    cards = []
+    sum = 0
+    hasBlackjack = false
+    newCard()
+    newCard()
+    renderGame()
+}
 
 //starter code
     message = "Do you want to draw a new card? 🙂"
