1271d07841860820916a30ef09b4a79596b0d467
Jack Ni <a@a>
2024-10-28 21:06:41 -0400

    write functions next

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ let firstCard = 10
 let secondCard = 4
 
 //Part A and B in JS
-let cardHolder = document.createElement("p")
+/*let cardHolder = document.createElement("p")
 cardHolder.id = "cards-el"
 let sumHolder = document.createElement("p")
 sumHolder.id = "sum-el"
@@ -19,20 +19,33 @@ document.body.appendChild(startGame)
 document.body.appendChild(newCard)
 
 document.body.style.fontFamily = "Verdana, Geneva, Tahoma, sans-serif"
-document.body.style.backgroundImage = "images/table.png"
+document.body.style.background ="url('images/table.png')"
 document.body.style.textAlign = "center"
 document.body.style.color = "black"
 
-document.button.style.backgroundColor= "goldenrod";
-document.button.style.paddingtop = "8dp";
-document.button.style.margin = "10px";
-document.button.style.borderradius = "5px";
-document.button.style.width= "150px";
+document.getElementById("startGame").style.backgroundColor= "goldenrod";
+document.getElementById("startGame").style.paddingtop = "8dp";
+document.getElementById("startGame").style.margin = "10px";
+document.getElementById("startGame").style.borderradius = "5px";
+document.getElementById("startGame").style.width= "150px";
+document.getElementById("startGame").innerHTML = "Start game"
+
+document.getElementById("newCard").style.backgroundColor= "goldenrod";
+document.getElementById("newCard").style.paddingtop = "8dp";
+document.getElementById("newCard").style.margin = "10px";
+document.getElementById("newCard").style.borderradius = "5px";
+document.getElementById("newCard").style.width= "150px";
+document.getElementById("newCard").innerHTML = "New card"*/
 
 //challenge #1
 //create a new variable and set it to the sum of the two cards 
-
-
+let cards = []
+let sum = cards.sum
+let hasBlackjack = false
+let isAlive = true
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
 //chalenge #2 
 // code the statements below based on the conditions shown in blackjack.png from the images folder
 // your output should work in the browser's console based on changing the values assigned to the cards
@@ -42,3 +55,5 @@ document.button.style.width= "150px";
     message = "Wohoo! You've got Blackjack! 🥳"
     message = "You're out of the game! 😭"
 console.log(message)
+
+
